ce6eaeb7d624b80089a3a58d0d718ce6fa291196
Fauzan Emmerling <erich@emfeld.com>
2011-03-04 02:46:32 -0800

    adjusted the model

--- page/models.py
+++ page/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class News(models.Model):
-  news_id = models.AutoField(primary_key=True)
-  news_date = models.DateTimeField('date published')
-  news_title = models.CharField(max_length=200)
-  news_content = models.TextField()
-  news_status = models.CharField(max_length=1)
-  
-  def __unicode__(self):
-    return self.news_content
-    return self.news_title
-    return self.news_date
-      
 class Page(models.Model):
   page_id = models.CharField(max_length=20,primary_key=True)
   page_name = models.CharField(max_length=200)
